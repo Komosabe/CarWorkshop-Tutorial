c44415bb2c151b1d4b1489c74254eea6baedbabb
Maciek Pietrasz <73303606+Komosabe@users.noreply.github.com>
2023-08-28 20:54:59 +0200

    51. Validator tests

---
+++ CarWorkshop.ApplicationTests/CarWorkshopService/Commands/CreateCarWorkshopServiceCommandValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace CarWorkshop.Application.CarWorkshopService.Commands.Tests
+{
+    [TestClass()]
+    public class CreateCarWorkshopServiceCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            // arrenge 
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "100 PLN",
+                Description = "Description",
+                CarWorkshopEncodedName = "workshop1"
+            };
+
+            // act
+            var result = validator.TestValidate(command);
+
+
+            // assert 
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldHaveValidationError()
+        {
+            // arrenge 
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "",
+                Description = "",
+                CarWorkshopEncodedName = null
+            };
+
+            // act
+            var result = validator.TestValidate(command);
+
+
+            // assert 
+            result.ShouldHaveValidationErrorFor(c => c.Cost);
+            result.ShouldHaveValidationErrorFor(c => c.Description);
+            result.ShouldHaveValidationErrorFor(c => c.CarWorkshopEncodedName);
+        }
+    }
+}
